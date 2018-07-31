e5d0853a4a829eef5c580315ee65f7b198f7b677
Eric Hao <hao.awesome@gmail.com>
2018-07-31 13:28:25 -0700

    add diff class

---
+++ diff.py
@@ -0,0 +1,28 @@
+import difflib
+import string
+
+class Diff:
+    exclude = string.punctuation
+
+    def __init__(self, actual='', typed=''):
+        if actual:
+            self.set_actual(actual)
+        if typed:
+            self.set_typed(typed)
+
+    def set_actual(self, actual):
+        self.actual = ''.join([c for c in actual if c not in exclude]).split()
+
+    def set_typed(self, typed):
+        self.typed = ''.join([c for c in typed if c not in exclude]).split()
+
+    def generate_html(self):
+        diff = difflib.HtmlDiff().make_file(self.actual, self.typed)
+        f = open('diff.html', 'w')
+        f.write(diff)
+        f.close()
+        return diff
+
+if __name__ == '__main__':
+    d = Diff("I am trying to compare two text files and output the first string iasdn the comparison file that does not match buasdf t am having difficulty since I am very new to python. Can anybody please give me a sample way to use this module.", "I am trying to compare two text files and output the first string in the comparison file that does not match butm aving difficulty since I am very new to python. Can anybody pse give me a sample way to use this module.")
+    d.generate_html()
